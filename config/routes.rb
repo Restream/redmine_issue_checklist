909c7ee57be5ab1135e1f06bb768ec196b841b21
Daniel Ritz <daniel.ritz@alcatel-lucent.com>
2015-03-03 17:02:57 +0100

    Minimal Redmine 3.0 fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 
- match "checklist/done/:checklist_item_id" => 'issue_checklists#done'
- match "checklist/delete/:checklist_item_id" => 'issue_checklists#delete'
+ match "checklist/done/:checklist_item_id", to: 'issue_checklists#done', via: [:get, :post]
+ match "checklist/delete/:checklist_item_id", to: 'issue_checklists#delete', via: [:get, :post]
